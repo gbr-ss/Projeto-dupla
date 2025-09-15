2c8d2bd494e04f5037eafb7620b2d6b7f3c59f3a
gabriel Santos Silva <d12she12@gmail.com>
2025-09-15 16:41:51 -0300

    ultimo test

--- app.py
+++ app.py
@@ -89,4 +89,3 @@ for chute_historico, reacao_historico, foto_historico in st.session_state.histor
         if foto_historico:
             st.image(foto_historico, width=50)
     st.markdown("---")
-a
