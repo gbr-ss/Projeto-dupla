e15e56776ceeeb3a98f729d6e8420a3255f47f92
Cassio Rosa <cassioasrosa@gmail.com>
2025-09-15 16:26:57 -0300

    commit add

--- app.py
+++ app.py
@@ -89,3 +89,5 @@ for chute_historico, reacao_historico, foto_historico in st.session_state.histor
         if foto_historico:
             st.image(foto_historico, width=50)
     st.markdown("---")
+
+    print()
